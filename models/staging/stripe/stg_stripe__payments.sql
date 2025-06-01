8128278c69ecd296aeab038a62cf952de6a857a1
Alarmelu Pichu Mani <alarmelupm.dbt25@gmail.com>
2025-06-01 21:41:55 +0000

    basics commit

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,11 @@
+select
+    id as payment_id,
+    order_id as order_id,
+    payment_method as payment_method,
+    status,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created_at as created_at
+
+from raw.stripe.payment 
